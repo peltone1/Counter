d24af8d21ad3c8f315d935246a85ff25051922e1
MateuszPeliński <pelinski.m@gmail.com>
2018-08-02 15:45:00 +0200

    addition and subtraction

--- src/Counter.js
+++ src/Counter.js
@@ -1,24 +1,51 @@
 import React from 'react'
  class Counter extends React.Component {
     state = {
-            number: 0
+            number: 0,
+            numberTwo: this.number
         }
         incHandler = () => {
             this.setState({
                 number: this.state.number + 1
             })
         }
+        decHandler = () => {
+            this.setState({
+                number: this.state.number - 1
+            })
+        }
+        incFiveHandler = () => {
+            this.setState({
+                number: this.state.number + 5
+            })
+        }
+        decFiveHandler = () => {
+            this.setState({
+                number: this.state.number - 5
+            })
+        }
     
     render() {
         return (
             <div>
-                <h1>{this.state.number}</h1>
-                <button 
-                konClick={this.incHandler}
-                >
-                +
-                </button>
-                <button>-</button>
+                <div>
+                    <h1>{this.state.number}</h1>
+                    <button onClick={this.incHandler}>
+                    +1
+                    </button>
+                    <button onClick={this.decHandler}>
+                    -1
+                    </button>
+                </div>
+                <div>
+                    <button onClick={this.incFiveHandler}>
+                    +5
+                    </button>
+                    <button onClick={this.decFiveHandler}>
+                    -5
+                    </button>
+                </div>
+              
             </div>
         )
     }
